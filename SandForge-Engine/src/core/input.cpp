14c335cc9d609608a344a3ac1f338bfd63b65458
Marti Sabaté <71668554+msabate00@users.noreply.github.com>
2026-04-18 21:15:54 +0200

    ops

--- SandForge-Engine/src/core/input.cpp
+++ SandForge-Engine/src/core/input.cpp
@@ -4,6 +4,7 @@
 #include "../app/app.h"
 #include "../ui/ui.h"
 #include <algorithm>
+#include "core/particles.h"
 
 Input::Input(App* app, bool start_enabled) : Module(app, start_enabled) {};
 Input::~Input() = default;
